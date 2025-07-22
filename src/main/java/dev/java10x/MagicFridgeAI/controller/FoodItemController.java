af00703cf3921a6f0c05cd10f3ca9ac411557189
Navarro Rangel <navarrorangel1@hotmail.com>
2025-07-22 18:08:54 -0300

    :sparkles: feat: adicionando rotas de Listar por id, deletar Por id, e alterar fooditem

--- src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
@@ -6,7 +6,6 @@ import dev.java10x.MagicFridgeAI.service.FoodItemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,7 +31,26 @@ public class FoodItemController {
         return repository.findAll();
     }
 
+
+    //TODO: Listar por id
+    public ResponseEntity<FoodItemModel> listarPorId(@RequestBody Long id){
+        FoodItemModel buscaId = service.buscaId(id);
+        return ResponseEntity.ok(buscaId);
+
+    }
+
+
     //Update
 
+    public ResponseEntity<FoodItemModel> alterar(@RequestBody FoodItemModel foodItemModel, Long id){
+        FoodItemModel alterado = service.altera(foodItemModel, id);
+        return ResponseEntity.ok(alterado);
+    }
+
     //DELTE
+
+    public ResponseEntity<String> deleteById(Long id){
+        service.deleta(id);
+        return ResponseEntity.ok("Deletado com sucesso o id " + id);
+    }
 }
