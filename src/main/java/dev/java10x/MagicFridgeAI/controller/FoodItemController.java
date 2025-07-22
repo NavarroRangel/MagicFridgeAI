ce9a7dcd80bbaef26b123e6a1360312cf3486002
Navarro Rangel <navarrorangel1@hotmail.com>
2025-07-22 17:44:13 -0300

    :sparkles: feat: criando controler e rota post

--- src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
@@ -1,4 +1,30 @@
 package dev.java10x.MagicFridgeAI.controller;
 
+import dev.java10x.MagicFridgeAI.model.FoodItemModel;
+import dev.java10x.MagicFridgeAI.service.FoodItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food")
 public class FoodItemController {
+    private FoodItemService service;
+
+    public FoodItemController(FoodItemService foddItemService) {
+        this.foddItemService = foddItemService;
+    }
+    //POST
+    public ResponseEntity<FoodItemModel> criar(@RequestBody FoodItemModel foodItemModel){
+        FoodItemModel salvo = service.salvar(foodItemModel);
+        return ResponseEntity.ok(salvo);
+    }
+
+    //GET
+
+    //Update
+
+    //DELTE
 }
