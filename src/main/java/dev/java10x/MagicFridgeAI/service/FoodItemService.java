146d1d557304adb6aac49d48b13ebfc8e2fb6fc2
Navarro Rangel <navarrorangel1@hotmail.com>
2025-07-22 17:54:29 -0300

    :sparkles: feat: adicionando metodo salvar para a requisição POST e o listar para a requisição GET

--- src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
+++ src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
@@ -1,4 +1,25 @@
 package dev.java10x.MagicFridgeAI.service;
 
+import dev.java10x.MagicFridgeAI.model.FoodItemModel;
+import dev.java10x.MagicFridgeAI.repository.FoodItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FoodItemService {
+
+    private FoodItemRepository repository;
+
+    public FoodItemService(FoodItemRepository repository) {
+        this.repository = repository;
+    }
+
+    public FoodItemModel salvar(FoodItemModel foodItemModel){
+        return repository.save(foodItemModel);
+    }
+
+    public List<FoodItemModel> listar(){
+        return repository.findAll();
+    }
 }
