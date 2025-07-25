8102ca5c74df6498f52c687b5ef899393b824d07
Navarro Rangel <navarrorangel1@hotmail.com>
2025-07-25 11:00:30 -0300

    :sparkles: feat: adicionando fooditemlist para receber os igredientes do banco de dados

--- src/main/java/dev/java10x/MagicFridgeAI/controller/RecipeController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/RecipeController.java
@@ -1,23 +1,31 @@
 package dev.java10x.MagicFridgeAI.controller;
 
+import dev.java10x.MagicFridgeAI.model.FoodItemModel;
+import dev.java10x.MagicFridgeAI.service.FoodItemService;
 import dev.java10x.MagicFridgeAI.service.GeminiService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @RestController
 public class RecipeController {
 
+    private FoodItemService foodItemService;
+
     private GeminiService geminiService;
 
-    public RecipeController(GeminiService geminiService) {
+    public RecipeController(FoodItemService foodItemService, GeminiService geminiService) {
+        this.foodItemService = foodItemService;
         this.geminiService = geminiService;
     }
 
     @GetMapping("/generate")
     public Mono<ResponseEntity<String>> generateRecipe(){
-        return geminiService.generateRecipe()
+        List<FoodItemModel> foodItemModelList = foodItemService.listar();
+        return geminiService.generateRecipe(foodItemModelList)
                 .map(recipe -> ResponseEntity.ok(recipe))
                 .defaultIfEmpty(ResponseEntity.noContent().build());
     }
